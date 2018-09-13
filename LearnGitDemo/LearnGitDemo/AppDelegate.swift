31bb87715aac05d11e4235fd61ef126a9c7f3900
jianghaiqing <68171733@qq.com>
2018-09-13 22:38:30 +0800

    First Commit

---
+++ LearnGitDemo/LearnGitDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LearnGitDemo
+//
+//  Created by jianghaiqing on 2018/9/13.
+//  Copyright © 2018年 jianghaiqing. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LearnGitDemo/LearnGitDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LearnGitDemo
+//
+//  Created by jianghaiqing on 2018/9/13.
+//  Copyright © 2018年 jianghaiqing. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
